13de5237edd78e60582e69fde8bb5b5b60b2c56b
nikolaischunk <nikolai.schunk@gmail.com>
2023-07-04 00:59:30 +0200

    More improvements to the base

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
-import styles from "styles/components/main.module.scss";
-
 export default function HomePage() {
-  return (
-    <main className={styles.main}>
-      <h1 className={`${styles.title} is-size-1 is-color-primary`}>
-        Welcome to Birthdayy
-      </h1>
-      <button className="button is-danger ">Button</button>
-    </main>
-  );
+	return (
+		<main className="homePage">
+			<h1 className={`is-size-1 is-color-primary`}>Welcome to Birthdayy</h1>
+			<button className="button is-danger ">Button</button>
+		</main>
+	);
 }
