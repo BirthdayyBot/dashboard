2f670ee2546de62e04ea77aa3b7731bc31bc2164
nikolaischunk <nikolai.schunk@gmail.com>
2023-07-13 14:34:22 +0200

    feat: basic navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,7 +1,5 @@
 'use client';
-import { isActiveAtome } from '@lib/atoms';
 import { BIRTHDAYY_LOGO, DISCORD_BOT_NAME } from '@lib/environment';
-import { useAtom } from 'jotai';
 import { useSession } from 'next-auth/react';
 import Image from 'next/image';
 
@@ -13,15 +11,19 @@ https://daisyui.com/components/drawer/#navbar-menu-for-desktop--sidebar-drawer-f
 */
 
 export default function Navbar(): JSX.Element {
-	const [isResponsiveActiv, setIsResponsiveActiv] = useAtom(isActiveAtome);
+	// const [isResponsiveActiv, setIsResponsiveActiv] = useAtom(isActiveAtome);
 	const { data: session } = useSession();
 	const isLoggedIn = session ? true : false;
 
 	return (
 		<div className="navbarComponent">
 			<nav className="navbar bg-base-100">
-				<Image src={BIRTHDAYY_LOGO} width={64} height={64} alt="Birthdayy Logo" />
-				<a className="btn btn-ghost text-xl uppercase font-bold font-heading">{DISCORD_BOT_NAME}</a>
+				<div className="navbar-start">
+					<Image src={BIRTHDAYY_LOGO} width={64} height={64} alt="Birthdayy Logo" />
+					<a className="btn btn-ghost text-xl uppercase font-bold font-heading">{DISCORD_BOT_NAME}</a>
+				</div>
+				<div className="navbar-center">Middle</div>
+				<div className="navbar-end">{isLoggedIn ? 'Logged in' : 'Not logged in'}</div>
 			</nav>
 		</div>
 	);
