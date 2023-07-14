cecdfe02ef45368ff076e5b399fc7151abdba789
nikolaischunk <nikolai.schunk@gmail.com>
2023-07-14 13:25:34 +0200

    chore: add `nextauth_url` parameter to env (WIP)
    Related to Update to use /dashboard subpath #9

--- src/lib/environment.ts
+++ src/lib/environment.ts
@@ -1,8 +1,11 @@
 // Discord
 export const { DISCORD_CLIENT_SECRET } = process.env;
-export const DISCORD_CLIENT_ID = process.env.NEXT_PUBLIC_DISCORD_BOT_NAME;
+export const DISCORD_CLIENT_ID = process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID;
 
 // Webpage
 export const BRAND_COLOR = process.env.NEXT_PUBLIC_BRAND_COLOR;
 export const BIRTHDAYY_LOGO = process.env.NEXT_PUBLIC_BIRTHDAYY_LOGO;
 export const DISCORD_BOT_NAME = process.env.NEXT_PUBLIC_DISCORD_BOT_NAME;
+
+// NEXT
+// export const { NEXTAUTH_BASE_PATH, NEXTAUTH_URL } = process.env;
--- src/lib/types/Augments.d.ts
+++ src/lib/types/Augments.d.ts
@@ -4,6 +4,7 @@ declare namespace NodeJS {
 		NODE_ENV: 'development' | 'production';
 		APP_ENV: 'dev' | 'tst' | 'prd';
 		DEBUG: 'true' | 'false';
+		BASE_URL: string;
 
 		// Discord
 		DISCORD_CLIENT_SECRET: string;
@@ -13,5 +14,9 @@ declare namespace NodeJS {
 		// Webpage
 		NEXT_PUBLIC_BRAND_COLOR: string;
 		NEXT_PUBLIC_BIRTHDAYY_LOGO: string;
+
+		// NEXT
+		// NEXTAUTH_BASE_PATH: string;
+		// NEXTAUTH_URL: string;
 	}
 }
