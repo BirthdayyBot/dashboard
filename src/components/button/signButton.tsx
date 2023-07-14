6138e8d11c45ba34b5bd7178fa98916d21341ca7
nikolaischunk <nikolai.schunk@gmail.com>
2023-07-14 14:37:31 +0200

    feat: use correct login logout function

--- src/components/button/signButton.tsx
+++ src/components/button/signButton.tsx
@@ -1,26 +1,15 @@
 import { signIn, signOut } from 'next-auth/react';
 
-function login() {
-	signIn('discord').catch((err) => {
-		console.log(err);
-	});
-}
-
-function logout() {
-	signOut().catch((err) => {
-		console.log(err);
-	});
-}
-
-const LoginLogoutButton = ({ isLoggedIn }: { isLoggedIn: boolean }): JSX.Element => {
+const LoginLogoutButton = ({ isLoggedIn }: { isLoggedIn: boolean }) => {
+	console.log('LoginLogoutButton ~ isLoggedIn:', isLoggedIn);
 	if (isLoggedIn)
 		return (
-			<button onClick={() => login()} className={`btn is-rounded is-strong `}>
+			<button onClick={() => void signOut()} className={`btn is-rounded is-strong`}>
 				Logout
 			</button>
 		);
 	return (
-		<button onClick={() => logout()} className={`btn is-rounded is-strong $`}>
+		<button onClick={() => void signIn('discord')} className={`btn is-rounded is-strong`}>
 			Login
 		</button>
 	);
