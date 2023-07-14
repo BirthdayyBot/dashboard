501ca2ce1b13f7d024f0e427cf7035fa417f0ac7
nikolaischunk <nikolai.schunk@gmail.com>
2023-07-14 02:23:41 +0200

    feat: add secrets and profile to token & session

--- src/lib/options/auth.ts
+++ src/lib/options/auth.ts
@@ -1,13 +1,14 @@
-import { BIRTHDAYY_LOGO, BRAND_COLOR } from '@lib/environment';
-import type { NextAuthOptions } from 'next-auth';
-import DiscordProvider from 'next-auth/providers/discord';
+import { BIRTHDAYY_LOGO, BRAND_COLOR, DISCORD_CLIENT_ID } from '@lib/environment';
+import type { NextAuthOptions, Secrets } from 'next-auth';
+import type { DefaultJWT } from 'next-auth/jwt';
+import DiscordProvider, { type DiscordProfile } from 'next-auth/providers/discord';
 
-const scopes = ['identify'].join(' ');
+const scopes = ['identify', 'guilds'].join(' ');
 export const authOptions: NextAuthOptions = {
 	// https://next-auth.js.org/configuration/providers/oauth
 	providers: [
 		DiscordProvider({
-			clientId: process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID,
+			clientId: DISCORD_CLIENT_ID,
 			clientSecret: process.env.DISCORD_CLIENT_SECRET,
 			authorization: { params: { scope: scopes } }
 		})
@@ -20,12 +21,33 @@ export const authOptions: NextAuthOptions = {
 	},
 	callbacks: {
 		session: ({ session, token }) => {
+			console.log('session ~ token:', token);
 			if (session?.user) {
 				session.user.userId = token.sub;
 				session.user.username = token.name;
 				session.user.imageUrl = token.picture;
 			}
+			// Somewhat hacky implementation
+			session.profile = token.profile as DiscordProfile;
+			session.secrets = token.secrets as Secrets;
 			return session;
+		},
+		jwt({ token, user, profile, account }): DefaultJWT {
+			if (account) {
+				token.secrets = {
+					accessToken: account.access_token,
+					refreshToken: account.refresh_token,
+					expires_at: account.expires_at
+				};
+			}
+			if (profile) {
+				token.profile = profile;
+			}
+			console.log('jwt ~ token:', token);
+			console.log('jwt ~ user:', user);
+			console.log('jwt ~ profile:', profile);
+			console.log('jwt ~ account:', account);
+			return token;
 		}
 	},
 	session: {
--- src/lib/types/next-auth.d.ts
+++ src/lib/types/next-auth.d.ts
@@ -1,16 +1,37 @@
 import 'next-auth';
+import { DiscordProfile } from 'next-auth/providers/discord';
 // Read more at: https://next-auth.js.org/getting-started/typescript#module-augmentation
 // import type { DefaultUser } from 'next-auth';
 
 declare module 'next-auth' {
-	interface Session {
-		user: {
-			userId: string | undefined;
-			username: string | null | undefined;
-			imageUrl: string | null | undefined;
-		};
+	export interface Session {
+		user: User;
+		profile: DiscordProfile;
+		secrets: Secrets;
 	}
-	// interface JWT {
-	// 	userRole: 'admin';
+	interface JWT {
+		secrets: string;
+	}
+
+	export interface Secrets {
+		accessToken: string | undefined;
+		refreshToken: string | undefined;
+		expires_at: number | undefined;
+	}
+	// interface Profile {
+	// 	userId: string;
+	// 	username: string;
+	// 	discriminator: string;
+	// 	avatarUrl: string;
+	// 	avatar?: string | null;
+	// 	accentColor: number;
+	// 	locale: string;
+	// 	bannerUrl?: string;
 	// }
+
+	interface User {
+		userId: string | undefined;
+		username: string | null | undefined;
+		imageUrl: string | null | undefined;
+	}
 }
