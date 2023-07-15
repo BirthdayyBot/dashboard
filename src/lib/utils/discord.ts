9032afd183492cf738d71da011d1724f7efce8e3
nikolaischunk <nikolai.schunk@gmail.com>
2023-07-15 16:29:19 +0200

    feat: switch to `discordjs/core` usage

--- src/lib/utils/discord.ts
+++ src/lib/utils/discord.ts
@@ -1,14 +1,9 @@
+import { API } from '@discordjs/core';
 import { REST } from '@discordjs/rest';
-import { Routes } from 'discord-api-types/rest/v10';
 
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_BOT_TOKEN);
+const api = new API(rest);
 
 export async function getGuildInfo(guildId: string) {
-	try {
-		const guildData = await rest.get(Routes.guild(guildId));
-		return guildData;
-	} catch (error) {
-		console.error(error);
-		return null;
-	}
+	return api.guilds.get(guildId);
 }
