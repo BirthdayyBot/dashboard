e63bb79daa3d18baa5b6acbd631505ff611d01f5
nikolaischunk <nikolai.schunk@gmail.com>
2023-07-17 16:42:59 +0200

    fix: :bug: Change mock type

--- src/lib/utils/oauth.tsx
+++ src/lib/utils/oauth.tsx
@@ -1,11 +1,9 @@
-import { USE_MOCK } from '@lib/environment';
-import { GuildsMock } from '@lib/mock/guilds.mock';
 import DiscordOauth2 from 'discord-oauth2';
 
 const oauth = new DiscordOauth2();
 
 export async function getUserGuilds(accessToken: string) {
-	if (USE_MOCK) return GuildsMock;
+	// if (USE_MOCK) return GuildsMock;
 	return oauth.getUserGuilds(accessToken);
 }
 
