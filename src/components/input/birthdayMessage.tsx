6d97becbcfb03c3ebb592b963410769a82574799
nikolaischunk <58025175+nikolaischunk@users.noreply.github.com>
2023-09-29 00:46:18 +0200

    adjust birthday message premium condition

--- src/components/input/birthdayMessage.tsx
+++ src/components/input/birthdayMessage.tsx
@@ -5,21 +5,22 @@ export default function BirthdayMessageComponent({
 	announcementMessage: string;
 	isPremium: boolean | undefined;
 }) {
+	// isPremium = false;
 	return (
 		<div className="BirthdayMessageComponent">
 			<div className="form-control">
 				<label className="label">
 					<span className="label-text">Birthday Announcement Message</span>
 				</label>
-
 				{isPremium ? null : <div className="badge badge-outline badge-error">Premium Only</div>}
 				<textarea
 					className="textarea textarea-bordered h-24"
 					placeholder="Birthday Message"
 					defaultValue={announcementMessage}
 					content={announcementMessage}
-					contentEditable={isPremium}
-					// disabled={!isPremium}
+					disabled={!isPremium}
+					maxLength={512}
+					style={{ resize: 'none' }}
 				></textarea>
 			</div>
 		</div>
