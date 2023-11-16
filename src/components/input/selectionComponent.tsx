a0cb9d090d03f6094fedfc4120b3c3be7e91ad49
nikolaischunk <58025175+nikolaischunk@users.noreply.github.com>
2023-11-16 17:50:24 +0100

    Fix Selection Component behaviour

--- src/components/input/selectionComponent.tsx
+++ src/components/input/selectionComponent.tsx
@@ -1,5 +1,6 @@
 'use client';
 import type { RESTGetAPIGuildChannelsResult, RESTGetAPIGuildRolesResult } from 'discord-api-types/v10';
+import { ChangeEvent, useState } from 'react';
 
 interface DiscordSelectionComponentType {
 	type: 'channel' | 'role';
@@ -12,24 +13,57 @@ interface DiscordSelectionComponentType {
 // TODO: #23 Save that something changed in a state and check it if form is saved
 
 export default function DiscordSelectionComponent(params: DiscordSelectionComponentType) {
-	const defaultOption = params.id ? params.id : 'Select a channel';
+	const defaultOption: string = params.id === null ? '' : params.id;
+
+	const [selection, setSelection] = useState(defaultOption);
+	const [isChanged, setIsChanged] = useState(false);
+
+	function handleSelectionChange(event: ChangeEvent<HTMLSelectElement>): void {
+		if (event.target.value === defaultOption) {
+			setIsChanged(false);
+			setSelection(defaultOption);
+		} else {
+			setIsChanged(true);
+			setSelection(event.target.value);
+		}
+		return console.log(event.target.value);
+	}
+
 	return (
 		<div className="DiscordSelectionComponent">
 			<div className="form-control w-full max-w-xs pb-3">
 				<label className="label">
 					<span className="label-text font-sub-heading">{params.label}</span>
 				</label>
-				<select className="select select-bordered" defaultValue={defaultOption} onChange={(event) => console.log(event.target.value)}>
-					{params.data.map((data) => {
-						const isActiveValue: boolean = data.id === params.id;
-						if (data.id === params.guildId) return null;
-						return (
-							<option key={data.id} value={data.id} disabled={isActiveValue}>
-								{data.name}
-							</option>
-						);
-					})}
-				</select>
+				<div className="join">
+					<select
+						className="select select-bordered join-item"
+						defaultValue="Select a channel"
+						value={selection}
+						onChange={(event) => handleSelectionChange(event)}
+					>
+						<option value="" disabled>
+							Select a {params.type}
+						</option>
+						{params.data.map((data) => {
+							if (data.id === params.guildId) return null;
+
+							return (
+								<option key={data.id} value={data.id}>
+									{data.name}
+								</option>
+							);
+						})}
+					</select>
+					<button className="btn bg-primary join-item" disabled={!isChanged}>
+						Save
+					</button>
+				</div>
+				Selection: {selection}
+				<br />
+				isChanged: {isChanged.toString()}
+				<br />
+				id: {params.id}
 			</div>
 		</div>
 	);
